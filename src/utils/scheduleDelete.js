10b91a097d4b9d96b554b9633ed37829f38d27a7
Renan Cesar <cesar.renan@hotmail.com>
2024-04-07 02:35:08 -0300

    fix: cron job

--- src/utils/scheduleDelete.js
+++ src/utils/scheduleDelete.js
@@ -2,7 +2,8 @@ import cron from 'node-cron';
 import { deleteFreshTalents } from '../middlewares/global.middlewares.js';
 
 const scheduleDelete = () => {
-    const frequency = '0 0 0,6,12,18 * * *';
+    // const frequency = '0 0 0,6,12,18 * * *';
+    const frequency = '0 37 2 * * *';
 
     cron.schedule(frequency, async () => {
         console.log('Executando tarefa agendada para apagar talentos antigos.');
